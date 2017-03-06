8bd6a358a77d9bbbb1f51f577cdb38f7e88e6425
Priscilla Mannuel <pmannuel@gmail.com>
2017-03-06 13:29:44 -0500

    first excercise

---
+++ exercise1.rb
@@ -0,0 +1,15 @@
+#My first Ruby comment
+
+=begin
+      making a long comment
+      another comment
+=end
+      
+puts "hello"
+puts "Coding"
+puts "Dojo"
+
+print "This is not"
+print "a new line"
+
+puts "Cool"
